3da31701fd749659fea69a932d48ca7814ce6958
RemiVR <remi.vr5@gmail.com>
2015-02-03 17:53:44 +0100

    stored 2 links in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Url.create url: "https://www.google.es/"
+Url.create url: "https://www.facebook.com/"
