c503032f7d2ed4a4060a295225fe37988c7e81ec
RemiVR <remi.vr5@gmail.com>
2015-02-04 20:56:19 +0100

    saved the url into database

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,7 @@
 class UrlsController < ApplicationController
+	def index
+		@url = Url.order("created_at DESC")
+	end
 	def show
     	@url = Url.find(params[:urls_id])
     	redirect_to @url.url
@@ -7,11 +10,15 @@ class UrlsController < ApplicationController
 		@url = Url.new
 	end
 	def create
-		@url = Url.new
-		if @entry.save
-			redirect_to action: 'show', controller: 'erls'
+		@url = Url.new entry_params
+		if @url.save
+			redirect_to action: 'index', controller: 'urls'
 		else
 			render 'new'
 		end
+	end
+	private
+	def entry_params
+		params.require(:url).permit(:url)
 	end
 end
